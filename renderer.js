c0a7da055b07d0956be0c5ee5d55f9a42d065844
husseinhareb <shtam.hareb12@gmail.com>
2023-11-18 01:23:21 +0200

    renderer

--- renderer.js
+++ renderer.js
@@ -1,73 +1,45 @@
-function createNote() {
-  // Create a div element
-  const divNote = document.createElement('div');
-  divNote.className = 'divNote';
+let { ipcRenderer } = require("electron");
 
-  // Create an input element
-  const inputText = document.createElement('input');
-  inputText.type = 'text';
-  inputText.placeholder = 'Enter Your Note...';
-  inputText.className = 'inputText';
-  
-  // Create a container for buttons
-  const buttonsContainer = document.createElement('div');
-  buttonsContainer.className = 'buttonsContainer';
+let title = document.getElementById("title");
+let note = document.getElementById("note");
+let btn = document.getElementById("addNote");
+let list = document.getElementById("list");
 
-  // Create the first button (SAVE or EDIT)
-  const saveEditButton = document.createElement('button');
-  saveEditButton.innerHTML = '<a href="#"><span><i class="fas fa-save"></i></span></a>'; // Font Awesome save icon
-  saveEditButton.className = 'saveEditButton';
+let notes = [];
 
-  // Create the second button (DELETE) using Font Awesome icons
-  const deleteButton = document.createElement('button');
-  deleteButton.innerHTML = '<a href="#"><span><i class="fa fa-trash"></i></span></a>'; // Font Awesome trash icon
-  deleteButton.className = 'deleteButton';
-  // Function to toggle between SAVE and EDIT
-  saveEditButton.addEventListener('click', function() {
-
-    const icon = saveEditButton.querySelector('i');
-
-    if (icon.classList.contains('fa-save')) {
-      inputText.disabled = true;
-      icon.classList.remove('fa-save');
-      icon.classList.add('fa-pencil-alt');
-    } else {
-      inputText.disabled = false;
-      icon.classList.remove('fa-pencil-alt');
-      icon.classList.add('fa-save');
-    }
+function loadNotes() {
+  list.innerHTML = "";
+  notes.forEach((note, idx) => {
+    list.innerHTML += `
+    <div class="divNote">
+      <input type="text" class="noteInput" placeholder="Enter Your Note..." value="${idx}- ${note.note}">
+      <div class="buttonsContainer">
+        <button class="editNote"><a href="#"><span><i class="fa fa-pencil-alt"></i></span></a></button>
+        <button class="deleteNote" data-index="${idx}"><a href="#"><span><i class="fa fa-trash"></i></span></a></button>
+      </div>
+    </div>`;
   });
-
-  // Function to delete divNote on DELETE click
-  deleteButton.addEventListener('click', function() {
-    divNote.classList.add('fade-out');
-  
-    divNote.addEventListener('transitionend', function() {
-      divNote.remove();
-    });
-  
-  
-    setTimeout(() => {
-      divNote.remove();
-    }, 500); 
-  });
-  
-  
-  divNote.appendChild(inputText);
-  buttonsContainer.appendChild(deleteButton);
-  divNote.appendChild(buttonsContainer);
-  buttonsContainer.appendChild(saveEditButton);
-
-  divNote.classList.add('new-note');
-
-  setTimeout(() => {
-    divNote.classList.add('show');
-    
-    divNote.scrollIntoView({ behavior: 'smooth', block: 'start' });
-  }, 10);
-
-  const notesContainer = document.getElementById('notesContainer');
-  notesContainer.appendChild(divNote);
 }
+
+window.onload = async () => {
+  notes = await ipcRenderer.invoke("get_data");
+  loadNotes();
+};
+
+btn.onclick = () => {
+  if (title !== "" && note !== "") {
+    let _note = {
+      note: note.value,
+    };
+
+    notes.push(_note);
+    loadNotes();
+
+    ipcRenderer.send("save_note", _note);
+  } else {
+    window.alert("please fill all the things and try again");
+  }
+};
+
 
 
