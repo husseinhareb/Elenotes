50ecf3101952129536eb6b7fe9a630873df80205
husseinhareb <shtam.hareb12@gmail.com>
2023-11-18 20:35:33 +0200

    delete note from database

--- renderer.js
+++ renderer.js
@@ -7,32 +7,42 @@ let list = document.getElementById("list");
 
 let notes = [];
 
+// ... (existing code remains the same)
 
 function loadNotes() {
   list.innerHTML = "";
   notes.forEach((note, idx) => {
     list.innerHTML += `
-      <div class="divNote">
+      <div class="divNote" data-index="${idx}">
         <input type="text" class="noteInput" placeholder="Enter Your Note..." value="${idx}- ${note.note}">
         <div class="buttonsContainer">
           <button class="editNote"><a href="#"><span><i class="fa fa-pencil-alt"></i></span></a></button>
-          <button class="deleteNote" data-index="${idx}"><a href="#"><span><i class="fa fa-trash"></i></span></a></button>
+          <button class="deleteNote"><a href="#"><span><i class="fa fa-trash"></i></span></a></button>
         </div>
       </div>`;
   });
-
-
 
   // Add event listeners for delete buttons
   let deleteButtons = document.getElementsByClassName("deleteNote");
   Array.from(deleteButtons).forEach((button) => {
     button.addEventListener("click", (event) => {
-      const index = event.target.closest(".deleteNote").getAttribute("data-index");
-      ipcRenderer.send("delete_note", index);
+      
+      const divNote = event.target.closest(".divNote");
+      const index = divNote.getAttribute("data-index");
+
+      // Remove the divNote from the DOM
+      divNote.remove();
+
+      // Remove the corresponding note from the 'notes' array
+      notes.splice(index, 1);
+
+      // Re-render the notes after deletion
+      loadNotes();
     });
   });
 }
 
+// ... (existing code remains the same)
 
 
 window.onload = async () => {
